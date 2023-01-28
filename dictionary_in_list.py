5981f5acb8903c799acf61052237b6b477928c44
DCL <raihan15-3098@diu.edu.bd>
2023-01-28 13:08:42 +0600

    use by list update

--- dictionary_in_list.py
+++ dictionary_in_list.py
@@ -1,3 +1,31 @@
-iuwksnksa
-aslnmlsaknlsam
-samnlnalsnlas
+travel_log=[ {"county":"banglasesh",
+              "visited":"1,2",
+              "city":["dhaka","mymenshingh","nowakhali"]
+
+
+},
+
+             {
+                 "county":"india",
+                 "visited":"1,2,3",
+                 "city":["kolikata","mumbai","rajisthan" ]
+
+
+             },
+
+]
+def add_new_county (visited_county,new_visited,visited_citys):
+    new_county={}
+
+    new_county["county"]=visited_county
+    new_county["visited"]=new_visited
+    new_county["city"]=visited_citys
+
+    travel_log.append(new_county)
+
+
+
+
+add_new_county(visited_county="russia",new_visited=2,visited_citys=["moscow","perent"])
+
+print(travel_log)
