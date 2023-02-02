2da0034cf3e9b0c56a0d9fbe27c5664d12e2f825
DCL <raihan15-3098@diu.edu.bd>
2023-02-02 19:40:13 +0600

    some_logic_of_global_and_local_scope

--- global&local_scope.py
+++ global&local_scope.py
@@ -1,35 +1,35 @@
-# mango=2 #its a global variable,
+mango=2 #its a global variable,
 
-# def food ():
-#     mango=10      #   its a local variable
-#     banana=4
-#     print(banana)
-#     print(mango)
-# food()
-# print(mango)
-
-
-# banana=200
-# apple=500
-# def shop():
-#     def food():
-#         banana=500
-#         print(banana)
-#         print(apple)
-#     food()
-# shop()
-# print(apple)
+def food ():
+    mango=10      #   its a local variable
+    banana=4
+    print(banana)
+    print(mango)
+food()
+print(mango)
 
 
+banana=200
+apple=500
+def shop():
+    def food():
+        banana=500
+        print(banana)
+        print(apple)
+    food()
+shop()
+print(apple)
 
 
-# gmae_lavel=3
-# def name():
-#     llist=["raihan","roman","ria","nodi"]
-#     if gmae_lavel==3:
-#         print(llist[0])
-# name()
-#
+
+
+gmae_lavel=3
+def name():
+    llist=["raihan","roman","ria","nodi"]
+    if gmae_lavel==3:
+        print(llist[0])
+name()
+
 
 enimes=2
 def enime_increased():
