2caaa91fa8e069b30bd0e85b2bbc9214139c888f
DCL <raihan15-3098@diu.edu.bd>
2023-02-02 19:04:31 +0600

    some_logic_of_global_and_local_scope

---
+++ global&local_scope.py
@@ -0,0 +1,33 @@
+# mango=2 #its a global variable,
+
+# def food ():
+#     mango=10      #   its a local variable
+#     banana=4
+#     print(banana)
+#     print(mango)
+# food()
+# print(mango)
+
+
+# banana=200
+# apple=500
+# def shop():
+#     def food():
+#         banana=500
+#         print(banana)
+#         print(apple)
+#     food()
+# shop()
+# print(apple)
+
+
+
+
+gmae_lavel=3
+def name():
+    llist=["raihan","roman","ria","nodi"]
+    if gmae_lavel==3:
+        print(llist[0])
+name()
+
+
