9baab7c519985e5eebc3aa9982328f3fb1718d4c
DCL <raihan15-3098@diu.edu.bd>
2023-02-12 07:13:05 +0600

    same code for next project

--- DAY_17-opp.py
+++ DAY_17-opp.py
@@ -3,14 +3,30 @@ class User:
         self.id = user_id
         self.name = user_name
         self.followers=0
+        self.following=0
+
+
+    def follow (self,user):
+        user.followers+=1
+        user.following+=1
 
 
 user1=User("001","rayhan")
 user_2=User("002","tushar")
+user1.follow(user_2)
 
-
-print(user1.id)
 print(user1.followers)
+print(user_2.followers)
+print(user1.following)
+print(user_2.following)
+
+
+
+
+
+
+
+
 
 
 
