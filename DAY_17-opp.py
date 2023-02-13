e86c12eb63e686e58abcf1ab235d470d51b931f3
DCL <raihan15-3098@diu.edu.bd>
2023-02-13 16:18:05 +0600

    quiz game

--- DAY_17-opp.py
+++ DAY_17-opp.py
@@ -20,6 +20,22 @@ print(user_2.followers)
 print(user1.following)
 print(user_2.following)
 
+# A Sample class with init method
+class Person:
+
+	# init method or constructor
+	def __init__(self, name):
+		self.name = name
+
+	# Sample Method
+	def say_hi(self):
+		print('Hello, my name is', self.name)
+
+
+p = Person('Nikhil')
+p.say_hi()
+
+
 
 
 
